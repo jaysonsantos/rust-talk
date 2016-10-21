7ccd716078bb216b700305e9f4bf8451ee579803
Jayson Reis <santosdosreis@gmail.com>
2016-10-21 09:12:17 +0200

    Change run.sh

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ else
     current_dir=$PWD
 fi
 sleep 10s
-docker run --name rust-talk --rm -v "$current_dir":/work -p3000:3000 --link rust-postgres:postgres rust-talk bash -c '/root/.cargo/bin/diesel migration run ; cargo run'
+docker run --name rust-talk --rm -v "$current_dir":/work -p3000:3000 --link rust-postgres:postgres --link rust-memcached:memcached rust-talk bash -c '/root/.cargo/bin/diesel migration run ; cargo run'
 read
 docker stop rust-talk
 docker rm rust-talk
